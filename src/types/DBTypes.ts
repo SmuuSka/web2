70181cd3237a6251889f30dde5435e9a80b79bf0
SmuuSka <samuaikio@gmail.com>
2024-01-19 13:23:04 +0200

    tests passed 10/20

---
+++ src/types/DBTypes.ts
@@ -0,0 +1,27 @@
+type User = {
+  user_id: number;
+  user_name: string;
+  email: string;
+  role: 'admin' | 'user';
+  password: string;
+};
+
+type Cat = {
+  // TODO: create a cat type
+  // owner should be a User or a number
+  cat_id: number;
+  cat_name: string;
+  weight: number;
+  filename: string;
+  birthdate: string;
+  lat: number;
+  lng: number;
+  owner: {
+    user_id: number;
+    user_name: string;
+  };
+};
+
+export {Cat};
+
+export {User};
