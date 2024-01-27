5c0b50485fee5fb46399fbcd45033afa48a09cc2
SmuuSka <samuaikio@gmail.com>
2024-01-27 12:56:39 +0200

    user auth fix

--- SSSF-week2-starter/src/api/routes/userRoute.ts
+++ SSSF-week2-starter/src/api/routes/userRoute.ts
@@ -1,13 +1,16 @@
 import express from 'express';
 import {
   checkToken,
+  userDelete,
   userDeleteCurrent,
   userGet,
   userListGet,
   userPost,
+  userPut,
   userPutCurrent,
 } from '../controllers/userController';
 import passport from '../../passport';
+import {body, param} from 'express-validator';
 
 const router = express.Router();
 
@@ -16,7 +19,13 @@ const router = express.Router();
 router
   .route('/')
   .get(userListGet)
-  .post(userPost)
+  .post(
+    passport.authenticate('jwt', {session: false}),
+    body('username').isLength({min: 3}),
+    body('email').isEmail(),
+    body('password').isLength({min: 3}),
+    userPost
+  )
   .put(passport.authenticate('jwt', {session: false}), userPutCurrent)
   .delete(passport.authenticate('jwt', {session: false}), userDeleteCurrent);
 
@@ -26,6 +35,18 @@ router.get(
   checkToken
 );
 
-router.route('/:id').get(userGet);
+router
+  .route('/:id')
+  .get(param('_id').isNumeric, userGet)
+  .put(
+    passport.authenticate('jwt', {session: false}),
+    param('_id').isNumeric(),
+    userPut
+  )
+  .delete(
+    passport.authenticate('jwt', {session: false}),
+    param('_id').isNumeric(),
+    userDelete
+  );
 
 export default router;
