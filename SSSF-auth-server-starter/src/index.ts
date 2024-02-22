39e64cf5cd9ccdd1e0f9ff0a5b4052ec34710e77
SmuuSka <samuaikio@gmail.com>
2024-02-22 15:36:43 +0200

    port fix

--- SSSF-auth-server-starter/src/index.ts
+++ SSSF-auth-server-starter/src/index.ts
@@ -1,7 +1,7 @@
 import app from './app';
 import mongoConnect from './utils/db';
 
-const port = process.env.PORT_AUTH || 3001;
+const port = process.env.PORT || 3001;
 (async () => {
   try {
     await mongoConnect();
